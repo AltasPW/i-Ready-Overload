4d8af8d16e61b65f874c2e048ae14d4e18151b39
AltasPW <121792353+AltasPW@users.noreply.github.com>
2023-01-12 19:03:07 -0600

    Update build.js

--- build.js
+++ build.js
@@ -5,5 +5,5 @@ if (document.scripts[15].src === scriptToReplace) {
     alert('The lesson has already been loaded. Please refresh the page and run the script before going into your lesson.')
 } else {
     // Override the script with a new one
-    fetch("https://raw.githubusercontent.com/AltasPW/i-Ready-Overload/main/main.script").then(response => response.text()).then(script => eval(script))
+    fetch("https://raw.githubusercontent.com/AltasPW/i-Ready-Overload/main/main.js").then(response => response.text()).then(script => eval(script))
 }
